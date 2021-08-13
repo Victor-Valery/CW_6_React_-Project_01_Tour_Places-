5554c58f541d4f64e49466616275a76e254fe841
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-08-13 16:32:16 +0200

    App_created

--- tour_places/src/App.js
+++ tour_places/src/App.js
@@ -1,11 +1,17 @@
 
 import './App.css';
+import Card from "../src/components/cards/Card";
+import Navbar from "../src/components/navbar/Navbar";
+import Header from "../src/components/header/Header";
+import {places} from "./helper/Data";
+
 
 function App() {
   return (
     <div className="App">
-      
-
+      <Navbar/>
+      <Header/>
+      {places.map(x =>(<Card placeName={x.placeName} placeImgSrc={x.placeImg} placeText={x.placeText}></Card>))}
     </div>
   );
 }
