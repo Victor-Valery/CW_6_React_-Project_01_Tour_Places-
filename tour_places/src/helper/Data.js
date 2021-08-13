06166e573c04b058e53a821a755d8e5aac67ff58
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-08-13 15:36:57 +0200

    Props_created

---
+++ tour_places/src/helper/Data.js
@@ -0,0 +1,83 @@
+export const places = [
+    {
+        placeName: "New York",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-new-york.jpg",
+        placeText: "New York City is like no other city in the world, and one that must be experienced to be fully appreciated. For first-time visitors, walking the streets can be like walking through a movie set, with famous sites at every turn, from the Empire State Building, to Rockefeller Plaza, the Chrysler Building, Central Park, Times Square, 5th Avenue, Broadway, and of course, the Statue of Liberty.",
+    },
+    {
+        placeName: "San Francisco",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-san-francisco.jpg",
+        placeText: "This charming and picturesque West Coast city is the perfect getaway destination for couples, singles, or families. Famous sites, charming streets, beautiful views, and outdoor dining are all part of what makes San Francisco such a great place to visit.",
+    },
+    {
+        placeName: "The Grand Canyon",
+        placeImg: "https://www.planetware.com/wpimages/2019/08/united-states-best-places-to-visit-grand-canyon-lookout.jpg",
+        placeText: "The Grand Canyon is one of those must-see, bucket-list destinations that have been attracting visitors for generations. Gazing out over the canyon walls to an endless horizon and an unfathomable depth below is one of the highlights of any trip in the United States.",
+    },
+    {
+        placeName: "Houston",
+        placeImg: "https://www.planetware.com/wpimages/2019/08/united-states-best-places-to-visit-grand-canyon-lookout.jpg",
+        placeText: "Houston is the perfect city for a friends getaway, a couples' retreat, or a family vacation. With direct flights from cities across the United States and Canada, you can easily fly in to see a sports game, wander through Houston's Museum District, lounge by a pool at one of the many luxury hotels, and spend your evenings enjoying incredible meals. Houston has developed into a hot spot for dining in the United States and is now well known for extraordinary cuisine.",
+    },
+    {
+        placeName: "Las Vegas",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-las-vegas.jpg",
+        placeText: "This glittering city of lights in the desert holds a unique appeal, and one that has been attracting visitors for decades. Huge resort complexes, with all kinds of things to see and do at any time of year, have made this a destination that draws everyone, from want-to-be newlyweds who come here to say their vows, to families who just want to hang out around a pool.",
+    },
+    {
+        placeName: "Waikiki",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-waikiki.jpg",
+        placeText: "Waikiki is one of America's top beaching destinations, with all the comforts of North America on a beautiful tropical island in the Pacific Ocean. Located on the Hawaiian island of Oahu, Waikiki is a suburb of Honolulu known for the beautiful golden sand beach that stretches along the oceanfront, backed by hotels and retail establishments.",
+    },
+    {
+        placeName: "Washington, D.C.",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-washington.jpg",
+        placeText: "Washington, D.C. is the US capitol and home to some of the most famous sites and national treasures in America, from the White House and the Capitol Building to the Smithsonian museums. This city should be on everyone's itinerary of the East Coast.",
+    },
+    {
+        placeName: "Miami",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-miami.jpg",
+        placeText: "The hot spot of southern Florida, Miami is more than just a great beaching destination. Wonderful beaches can be found all over Florida, but Miami offers an atmosphere like no other city in the state.",
+    },
+    {
+        placeName: "Los Angeles",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-los-angeles.jpg",
+        placeText: "Southern California has a culture all of its own, and Los Angeles is the epicenter of this unique area. The city has always been associated with glamour, with the suburbs of Hollywood, Beverly Hills, and Bel Air dominating pop culture. Travelers will find all kinds of vacation possibilities in the Los Angeles area.",
+    },
+    {
+        placeName: "New Orleans",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-new-orleans.jpg",
+        placeText: "New Orleans is like no other southern city. The mix of cultures, with strong French and Spanish influences, is more reminiscent of the Caribbean than the United States. Cajun and Creole cuisine, jazz music, and the architecture of the French Quarter set this city apart and make it a perfect destination for a long weekend getaway.",
+    },
+    {
+        placeName: "Utah's National Parks",
+        placeImg: "https://www.planetware.com/wpimages/2019/08/united-states-best-places-to-visit-utah-national-parks-arches.jpg",
+        placeText: "Southern Utah is home to a landscape like no other, with canyons, arches, natural amphitheaters, and oddly beautiful rock formations. Fortunately, Utah's parks offer access to the state's most incredible scenery.",
+    },
+    {
+        placeName: "Orlando/Kissimmee",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-orlando.jpg",
+        placeText: "Orlando, which for most tourists is synonymous with Kissimmee, is all about the theme parks; Walt Disney World Resort, Universal Studios, and SeaWorld are the big attractions. This is one of the best vacation spots in the US for families. Kids love it. But adults will also enjoy the parks, as well as the shopping, dining, golfing, and sunshine.",
+    },
+    {
+        placeName: "Chicago",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-chicago.jpg",
+        placeText: "Set on the shores of Lake Michigan, Chicago is a great summer destination, with parks and a beautiful waterfront. But its arts and cultural attractions, as well as fantastic shopping, make it a popular year-round destination.",
+    },
+    {
+        placeName: "Phoenix",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-phoenix.jpg",
+        placeText: "In the heart of the Southwestern US, Phoenix is a huge, sprawling city that attracts large numbers of northerners during the winter months, who come here to escape the cold weather. The city and surroundings, which include Scottsdale, Mesa, and several other townships, are known in particular for their golf resorts and spa retreats.",
+    },
+    {
+        placeName: "Savannah",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-savannah.jpg",
+        placeText: "Savannah is the quintessential small southern city, with huge moss-covered trees in the beautiful parks, old southern mansions, and a lovely waterfront area with candy stores and other unique shops in historic buildings. The pace is slow, and the people are friendly.",
+    },
+    {
+        placeName: "Boston",
+        placeImg: "https://www.planetware.com/photos-large/USNY/usa-best-places-boston.jpg",
+        placeText: "Located along the Eastern Seaboard, Boston is a city of history. The famous Freedom Trail leads past some of the city's most famous and historic sites, making sightseeing and navigating through the city extremely easy.",
+    },
+];
+
